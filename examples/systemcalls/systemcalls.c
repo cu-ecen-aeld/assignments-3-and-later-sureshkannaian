bf8869290f6a919725211f6fd61b875026bb5b5d
Suresh Kannaian <suresh.kannaian@colorado.edu>
2022-08-17 22:19:40 -0400

    pass-1

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,4 +1,10 @@
 #include "systemcalls.h"
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
 
 /**
  * @param cmd the command to execute with system()
@@ -16,6 +22,17 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
+   if(cmd == NULL)  
+   {
+    return false;
+   }
+   
+   int retVal = system(cmd);
+
+   if(retVal == -1 || retVal == 127)  
+   {
+        return false;
+   }
 
     return true;
 }
@@ -40,14 +57,81 @@ bool do_exec(int count, ...)
     va_start(args, count);
     char * command[count+1];
     int i;
+    printf("**********************************\n");
+    printf("Starting do_exec Test with %ld\n", (long) getpid());
+    printf("**********************************\n");
     for(i=0; i<count; i++)
     {
         command[i] = va_arg(args, char *);
+        //printf("command arg %d = %s\n", i, command[i]);
     }
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
-    command[count] = command[count];
+    //command[count] = command[count];
+
+
+        pid_t cpid, w;
+        int wstatus;
+        //printf("\nParent before fork\n");
+        cpid = fork();
+        if (cpid == -1) {
+            perror("fork");
+            
+            return false;
+        }
+
+
+        if (cpid == 0) {            /* Code executed by child */
+            //printf("Child PID is %ld\n", (long) getpid());
+
+            //printf("child before execv with command=%s\n", command[0]);
+            int retv = execv(command[0], command);
+            //printf("child after execv with command=-->%s<--\n", command[0]);
+            if (retv == -1)
+            {
+                printf("child execv returned error returning false\n");
+                exit(EXIT_FAILURE);
+            }
+
+        } else {                    /* Code executed by parent */
+            do {
+                //printf("\n======================\n");
+                //printf("Parent before wait\n");
+                w = waitpid(cpid, &wstatus, WUNTRACED | WCONTINUED);
+                //printf("Parent after wait\n");
+                if (w == -1) {
+                    perror("waitpid");
+                    return false;
+                }
+
+                if (WIFEXITED(wstatus)) {
+                    printf("Parent pid %ld had child exited, status=%d,=>%d\n", (long) getpid(), WEXITSTATUS(wstatus), wstatus);
+                    if(WEXITSTATUS(wstatus) != 0)
+                    {
+                        printf("Parent- returning false to  main\n");
+                        return false;
+                    }
+                    else 
+                    {
+                        printf("Parent- returning true to  main\n");
+                        return true;
+                    }
+                } else if (WIFSIGNALED(wstatus)) {
+                    //printf("Parent- child killed by signal %d\n", WTERMSIG(wstatus));
+                } else if (WIFSTOPPED(wstatus)) {
+                    //printf("Parent- child stopped by signal %d\n", WSTOPSIG(wstatus));
+                } else if (WIFCONTINUED(wstatus)) {
+                    //printf("parent - child continued\n");
+                }
+            } while (!WIFEXITED(wstatus) && !WIFSIGNALED(wstatus));
+
+            //exit(EXIT_SUCCESS);
+
+        }
+
+
+
 
 /*
  * TODO:
@@ -60,6 +144,7 @@ bool do_exec(int count, ...)
 */
 
     va_end(args);
+
 
     return true;
 }
@@ -92,6 +177,71 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
+
+        pid_t cpid, w;
+        int wstatus;
+        //printf("\nParent before fork\n");
+        cpid = fork();
+        if (cpid == -1) {
+            perror("fork");
+            
+            return false;
+        }
+
+
+        if (cpid == 0) {            /* Code executed by child */
+            //printf("Child PID is %ld\n", (long) getpid());
+
+            //printf("child before execv with command=%s\n", command[0]);
+	        int fd = open(outputfile, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+	        dup2(fd, 1);            
+            int retv = execv(command[0], command);
+            close(fd);
+            //printf("child after execv with command=-->%s<--\n", command[0]);
+            if (retv == -1)
+            {
+                printf("child execv returned error returning false\n");
+                exit(EXIT_FAILURE);
+            }
+
+        } else {                    /* Code executed by parent */
+            do {
+                //printf("\n======================\n");
+                //printf("Parent before wait\n");
+                w = waitpid(cpid, &wstatus, WUNTRACED | WCONTINUED);
+                //printf("Parent after wait\n");
+                if (w == -1) {
+                    perror("waitpid");
+                    return false;
+                }
+
+                if (WIFEXITED(wstatus)) {
+                    printf("Parent pid %ld had child exited, status=%d,=>%d\n", (long) getpid(), WEXITSTATUS(wstatus), wstatus);
+                    if(WEXITSTATUS(wstatus) != 0)
+                    {
+                        printf("Parent- returning false to  main\n");
+                        return false;
+                    }
+                    else 
+                    {
+                        printf("Parent- returning true to  main\n");
+                        return true;
+                    }
+                } else if (WIFSIGNALED(wstatus)) {
+                    //printf("Parent- child killed by signal %d\n", WTERMSIG(wstatus));
+                } else if (WIFSTOPPED(wstatus)) {
+                    //printf("Parent- child stopped by signal %d\n", WSTOPSIG(wstatus));
+                } else if (WIFCONTINUED(wstatus)) {
+                    //printf("parent - child continued\n");
+                }
+            } while (!WIFEXITED(wstatus) && !WIFSIGNALED(wstatus));
+
+            //exit(EXIT_SUCCESS);
+
+        }
+
+
+
 
     va_end(args);
 
