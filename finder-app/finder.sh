bef95d3ea56e10e1ded1066373ce4271245288f0
Suresh Kannaian <suresh.kannaian@colorado.edu>
2022-09-04 13:52:28 -0400

    another try

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-# Author: Suresh Kannaian
+
+error_str="Usage: finder.sh <directory path> <string to search>"
+
 # Arguments check
 if ! [ $# -eq 2 ] 
 then
-    echo "Error: Number of arguments is wrong"
+    echo "Error: improper number of arguments"
     echo $error_str
     exit 1
 fi
@@ -15,7 +17,7 @@ then
     exit 1
 fi
 
+
 num_files=$(find $1 -type f | wc -l)
 num_matches=$(find $1 -type f -exec grep $2 {} \; | wc -l)
-
-echo "The number of files are $numfiles and the number of matching lines are $num_matches"
+echo "The number of files are $num_files and the number of matching lines are $num_matches"
