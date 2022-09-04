627d9d99100155f73cec9c72c235b8cf64d3f026
Suresh Kannaian <suresh.kannaian@colorado.edu>
2022-09-04 16:45:13 -0400

    switch from bash to sh

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -21,3 +21,4 @@ fi
 num_files=$(find $1 -type f | wc -l)
 num_matches=$(find $1 -type f -exec grep $2 {} \; | wc -l)
 echo "The number of files are $num_files and the number of matching lines are $num_matches"
+
